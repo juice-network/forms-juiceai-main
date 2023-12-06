6a6f5aa36ac2a482c5b4025ea440faf8158158c3
kitharvey <kitharveycaubalejo@gmail.com>
2023-12-06 20:59:38 +0800

    fix redirect url

--- src/routes/forms/[form_id]/subscription/+page.server.ts
+++ src/routes/forms/[form_id]/subscription/+page.server.ts
@@ -12,6 +12,6 @@ export const actions = {
 			name: firstname + ' ' + lastname
 		});
 
-		throw redirect(303, `/subscription/payment?customer_id=${customer.id}`);
+		throw redirect(303, `/forms/${form_id}/subscription/payment?customer_id=${customer.id}`);
 	}
 } satisfies Actions;
